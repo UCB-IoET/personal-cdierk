0e74e6c8b0274f1ebbafea816335d06c59eee591
Christie Dierk <cdierk@elon.edu>
2015-02-09 10:06:03 -0800

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ioet_contrib
 
 Shared contrib code for the IoET course. To add your own code here, please fork this and submit a pull request
+# personal-cdierk
--- lib/i2creg.lua
+++ lib/i2creg.lua
@@ -15,7 +15,17 @@ function REG:r(reg)
     -- create array with address
     -- write address
     -- read register with RSTART
+    -- storm.i2c.read()
     -- check all return values
+    local arr = storm.array.create(1, storm.array.UINT8)
+    arr:set(1, reg)
+    local status = cord.await(storm.i2c.write, self.port + self.address, storm.i2c.START, arr)
+    print(string.format("Write to address 0x%02x: %d", self.address, status))
+    if (status ~= storm.i2c.OK) then return nil end
+    status = cord.await(storm.i2c.read, self.port + self.address, storm.i2c.RSTART + storm.i2c.STOP, arr)
+    print(string.format("Read from address 0x%02x: %d. Value: %d", self.address, status, arr:get(1)))
+    return arr:get(1)
+    
 end
 
 function REG:w(reg, value)
@@ -23,6 +33,11 @@ function REG:w(reg, value)
     -- create array with address and value
     -- write
     -- check return value
+    local arr = storm.array.create(2, storm.array.UINT8)
+    arr:set(1, reg)
+    arr:set(2, value)
+    local status = cord.await(storm.i2c.write, self.port + self.address, storm.i2c.START + storm.i2c.STOP, arr)
+    print(string.format("Write to address 0x%02x: %d", self.address, status))
 end
 
 return REG
